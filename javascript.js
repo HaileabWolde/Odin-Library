e038d7fb55e96189b5365cfd223514d7af02228f
Haileab <womahaileab1428@gmail.com>
2025-02-21 16:01:46 +0300

    added book card for the card component

--- javascript.js
+++ javascript.js
@@ -5,19 +5,33 @@ const title = document.getElementById('title')
 const form = document.getElementById('myform');
 const author = document.getElementById('author');
 const total = document.getElementById('total');
+const read = document.getElementById('read');
 const completed = document.getElementById('completed');
 const addBookBtn = document.getElementById('addBook');
 const pageError = document.getElementById('pageError');
 const authorError = document.getElementById('authorError');
+const bookcard = document.getElementsByClassName('bookcard');
 const myLibrary = [];
 const regex = /^[a-zA-Z]+$/;
 
-function Book(title, author, total, completed){
+function Book(title, author, total, completed, checked){
     this.title = title;
     this.author = author;
     this.total = total;
     this.completed = completed;
+    this.read = checked;
 }
+
+function createAnotherAdd(){
+    const newDiv = document.createElement("div");
+    newDiv.className = "bookcard";
+    const otherspan = document.createElement("span");
+    otherspan.id = "openDialog";
+    otherspan.textContent = "+";
+    newDiv.appendChild(otherspan);
+    bookcard[0].parentNode.insertBefore(newDiv, bookcard[0]);
+}
+
 function addBookToLibrary(event) {
     event.preventDefault();
     if(!regex.test(author.value) || !(author.value)){
@@ -37,7 +51,7 @@ function addBookToLibrary(event) {
         return;
     }
     else{
-        const book = new Book(title.value, author.value, total.value, completed.value);
+        const book = new Book(title.value, author.value, total.value, completed.value, read.checked);
         myLibrary.push(book);
         pageError.style.display = "none";
         completed.style.borderBottom = '2px solid #54ACDB';
@@ -45,6 +59,7 @@ function addBookToLibrary(event) {
         author.style.borderBottom = "2px solid #54ACDB";
         dialog.close();
         form.reset();
+        createAnotherAdd();
         // Further handling here
     }
 }
